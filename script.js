4b7244450b36d5e7d95702edee68c1107ac920e4
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-07-22 12:43:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
 //your code here
-window.onload= function(){
-    var image=document.querySelectorAll('.image');
-    var boxes=document.querySelectorAll('.boxes');
+var dragSrcElement = null;
 
-    image.addEventListener('dragstart',(e)=>{
-        console.log("dragstart");
-        
-        e.target.className+='.selected';
-        setTimeout(()=>{
-            e.target.className+='hide';
-        },0);
-       
-    });
+function handleDragStart(e) {
+  dragSrcElement = this;
 
-    image.addEventListener('dragend',()=>{
-        console.log("dragend");
-    });
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
 
-    boxes.forEach(box=>{
-        box.addEventListener('dragover',(e)=>{
-           e.preventDefault();
-        });
-        box.addEventListener('dragenter',()=>{
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
 
-        });
-        box.addEventListener('dragleave',()=>{
+  e.dataTransfer.dropEffect = 'move';
 
-        });
-        box.addEventListener('drop',()=>{
+  return false;
+}
 
-        });
-    
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
 
-        });
-    }
+  if (dragSrcElement !== this) {
+    dragSrcElement.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
+function handleDragEnd(e) {
+  var images = document.querySelectorAll('.image');
+  [].forEach.call(images, function(image) {
+    image.classList.remove('over');
+  });
+}
+
+var images = document.querySelectorAll('.image');
+[].forEach.call(images, function(image) {
+  image.addEventListener('dragstart', handleDragStart, false);
+  image.addEventListener('dragover', handleDragOver, false);
+  image.addEventListener('drop', handleDrop, false);
+  image.addEventListener('dragend', handleDragEnd, false);
+});
