0cb13d0037759b356554bf1c1e7587ed5f0bec53
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-07-22 11:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
-const divs = document.querySelectorAll('.image');
-
-let dragSrcElement = null;
-
-function handleDragStart(e) {
-  dragSrcElement = this;
-  e.dataTransfer.effectAllowed = 'move';
+//your code here
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+ 
+const images = document.querySelectorAll(".image");
+ 
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
-
-function handleDragOver(e) {
-  if (e.preventDefault) {
-    e.preventDefault();
-  }
-  e.dataTransfer.dropEffect = 'move';
-  return false;
+ 
+function allowDrop(e) {
+  e.preventDefault();
 }
-
-function handleDrop(e) {
-  if (e.stopPropagation) {
-    e.stopPropagation();
+ 
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
   }
-
-  if (dragSrcElement !== this) {
-    // Swap background image URLs
-    const tempBackground = this.style.backgroundImage;
-    this.style.backgroundImage = dragSrcElement.style.backgroundImage;
-    dragSrcElement.style.backgroundImage = tempBackground;
-  }
-
-  return false;
+ 
+  dragdrop(clone);
+ 
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+ 
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
 }
-
-divs.forEach(div => {
-  div.addEventListener('dragstart', handleDragStart, false);
-  div.addEventListener('dragover', handleDragOver, false);
-  div.addEventListener('drop', handleDrop, false);
-});
+ 
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+ 
+images.forEach(dragdrop);
