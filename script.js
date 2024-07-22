dd9e4d1226236551126b7937e4157ea18317ba2a
Lavanya-pg87 <lavanyagangadharan99@gmail.com>
2024-07-22 11:11:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+let dragindex=0;
+let clone=" ";
+function drag(e){
+e.dataTransfer.setData("text",e.target.id);
+}
+function allowDrop(e){
+e.preventDefault();
+}
+
+function drop(e){
+    e.preventDefault();
+    clone=e.target.cloneNode(true);
+    let data=e.dataTransfer.getData("text");
+    if(clone.id !== data) {
+    let nodelist=document.getElementById("parent").childNodes;
+    for(let i=0;i<nodelist.length;i++){
+    if(nodelist[i].id===data){
+    dragindex=i;}
+    }
+    document.getElementById("parent").replaceChild(document.getElementById(data),e.target);
+    document.getElementById("parent").insertBefore(clone,document.getElementById("parent").childNodes[dragindex]);
+    }
+    }
